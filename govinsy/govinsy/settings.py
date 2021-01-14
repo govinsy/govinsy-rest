e2aeacd07b77189b220b377db7a98ae28f79647b
Jack Kahuna Laguna <jklaguna@pop-os.localdomain>
2021-01-14 07:55:42 +0700

    Update

--- govinsy/govinsy/settings.py
+++ govinsy/govinsy/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'govinsy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'govinsy',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '/opt/lampp/var/mysql/mysql.sock',
+        'PORT': '3306',
     }
 }
 
